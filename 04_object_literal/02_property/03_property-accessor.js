aaa2a46b7c81f0e976af0bc6040b2022f41746e6
JIHWAN <jjjh0508@gmail.com>
2023-08-14 16:00:45 +0900

    누락분

--- 04_object_literal/02_property/03_property-accessor.js
+++ 04_object_literal/02_property/03_property-accessor.js
@@ -26,3 +26,14 @@ var obj = {
 
 // 프로퍼티 키가 식별자 네이밍 규칙을 준수하지 않는 이름일 경우 반드시 대괄호 표기법 사용
 console.log(obj["dash-key"]);
+
+// var  $엘리먼트 = document.getElementById;   $
+// var _시작 =  _ 는됨
+// var 변_수 됨
+// var 변수1 
+// const API_KEY 상수는 _
+
+//  앞 숫자는 안됨
+//특수문자 안됨 _ $ 말고
+// - 안됨
+// 
